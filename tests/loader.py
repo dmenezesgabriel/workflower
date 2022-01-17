6b6bd4d47beede1aa7d43e55c38f47d38aa80255
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-01-17 16:14:03 -0300

    Add test validate schema version type

--- tests/loader.py
+++ tests/loader.py
@@ -38,6 +38,24 @@ def test_validate_missing_version():
         loader.validate_schema(configuration_dict)
 
 
+def test_validate_invalid_version_type():
+    with pytest.raises(InvalidTypeError):
+        yaml_config = """
+        version: 1
+        workflow:
+          name: papermill_sample_date_trigger
+          jobs:
+            - name: "papermill_sample"
+              uses: papermill
+              input_path: ""
+              output_path: ""
+              trigger: cron
+              minute: "*/5"
+        """
+        configuration_dict = yaml.safe_load(yaml_config)
+        loader.validate_schema(configuration_dict)
+
+
 def test_validate_missing_workflow():
     with pytest.raises(InvalidSchemaError):
         yaml_config = """
