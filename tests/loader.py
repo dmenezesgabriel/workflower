9caf86b4fb940b176fae5e5b87e29d4545519d4d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-01-17 16:16:30 -0300

    Add test invalid workflow name gtype

--- tests/loader.py
+++ tests/loader.py
@@ -56,6 +56,24 @@ def test_validate_invalid_version_type():
         loader.validate_schema(configuration_dict)
 
 
+def test_validate_invalid_workflow_name_type():
+    with pytest.raises(InvalidTypeError):
+        yaml_config = """
+        version: "1.0"
+        workflow:
+          name: 123
+          jobs:
+            - name: "papermill_sample"
+              uses: papermill
+              input_path: ""
+              output_path: ""
+              trigger: cron
+              minute: "*/5"
+        """
+        configuration_dict = yaml.safe_load(yaml_config)
+        loader.validate_schema(configuration_dict)
+
+
 def test_validate_missing_workflow():
     with pytest.raises(InvalidSchemaError):
         yaml_config = """
