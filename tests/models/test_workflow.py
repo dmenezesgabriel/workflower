12b94d91d7d93f06fc87626fe4eb667e4fc72ce7
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-01-25 09:38:04 -0300

    Add workflow model test delete and get

--- tests/models/test_workflow.py
+++ tests/models/test_workflow.py
@@ -23,10 +23,35 @@ def test_create_workflow(connection):
     assert workflow.name == "test"
 
 
-def test_update_workflow(connection):
+def test_get_one(connection):
+    name = "test"
+    Workflow.create(name=name)
+    workflow = Workflow.get_one(name=name)
+    assert workflow.name == name
+
+
+def test_get_all(connection):
+    names = ["test1", "test2", "test3"]
+    for name in names:
+        Workflow.create(name=name)
+    workflows = Workflow.get_all()
+    created_workflow_names = [workflow.name for workflow in workflows]
+    assert all(
+        [workflow_name in created_workflow_names for workflow_name in names]
+    )
+
+
+def test_update_(connection):
     new_name = "changed_name"
     workflow_before_update = Workflow.create(name="test")
     id = workflow_before_update.id
     Workflow.update({"id": id}, {"name": new_name})
     workflow_after_update = Workflow.get_one(id=id)
     assert workflow_after_update.name == new_name
+
+
+def test_delete(connection):
+    name = "changed_name"
+    Workflow.create(name=name)
+    Workflow.delete(name=name)
+    assert Workflow.get_one(name=name) is None
