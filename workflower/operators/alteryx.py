6b3c46e8c8654f0f44a4ffd5625b2469fa246401
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-01-25 19:13:07 -0300

    Remove parser from alteryx

--- workflower/operators/alteryx.py
+++ workflower/operators/alteryx.py
@@ -2,7 +2,6 @@
 Alteryx helper module
 """
 import logging
-import re
 from subprocess import PIPE, STDOUT, CalledProcessError, Popen
 
 from workflower.operators.operator import BaseOperator
@@ -37,38 +36,7 @@ class AlteryxOperator(BaseOperator):
                         "utf-8", errors="ignore"
                     ).rstrip()
                     logger.info(decoded_line)
-                    output["logs"].append(cls.result_parser(decoded_line))
+                    output["logs"].append({"message": decoded_line})
             except CalledProcessError as e:
                 logger.error(f"{str(e)}")
         return str(output)
-
-    @classmethod
-    def result_parser(cls, output):
-        """
-        Parse shell execution of Alteryx workflow through
-        command line executable AlteryxEngineCmd.exe
-        """
-        warn = re.compile(r"\d+\swarnings")
-        error = re.compile(r"\d+\serrors")
-        conversion_err = re.compile(r"\d+\sfield conversion errors")
-        # seconds = re.compile(r"\d+\.\d\d\d")
-
-        try:
-            warnings = warn.findall(output)[0]
-        except Exception:
-            warnings = 0
-        try:
-            errors = error.findall(output)[0]
-        except Exception:
-            errors = 0
-        try:
-            conversion = conversion_err.findall(output)[0]
-        except Exception:
-            conversion = 0
-
-        return {
-            "Warnings": warnings,
-            "FieldConversionErrors": conversion,
-            "Errors": errors,
-            "Message": output,
-        }
