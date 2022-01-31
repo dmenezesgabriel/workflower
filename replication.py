43e61cfc75e9a0ad7d33d3f9224a9a725e753dfe
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-01-31 15:08:50 -0300

    Add database replication draft

---
+++ replication.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+
+from workflower.models.base import BaseModel, database
+
+replica_db = create_engine("sqlite:///replica-dev.sqlite")
+replica_db.connect()
+
+try:
+    BaseModel.metadata.create_all(bind=replica_db)
+except Exception as error:
+    print(error)
+
+database.connect()
+database.engine.execute("ATTACH DATABASE 'replica-dev.sqlite' as replica")
+
+
+# import sqlite3
+
+# replica_db = sqlite3.connect("file::memory:?cache=shared", uri=True)
+# # main_db = sqlite3.connect("app-dev.sqlite", uri=True)
+# replica_db.execute("ATTACH DATABASE 'app-dev.sqlite' AS master")
+# print(list(replica_db.execute("select * from master.jobs")))
