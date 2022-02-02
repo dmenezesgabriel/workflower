6d7bae896ffa79ec22090e4494a4008f73a1b700
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-02 18:38:20 -0300

    Update samples name to remove sample redundancy and alteryx email alert

--- samples/python/sys_argv/get_argv.py
+++ samples/python/sys_argv/get_argv.py
@@ -1,19 +1,5 @@
 import sys
 
-import win32com.client as win32
-
 print("This is the name of the program:", sys.argv[0])
 
 print("Argument List:", str(sys.argv))
-
-
-outlook = win32.Dispatch("outlook.application")
-mail = outlook.CreateItem(0)
-# Must write some destination mail address here
-mail.To = ""
-mail.Subject = "Python automation email test"
-mail.Body = "Hello from python\n" + str(sys.argv[1])
-# mail.HTMLBody = "<h2>Title</h2>"
-# attachment = "Path to the attachment"
-# mail.Attachments.Add(attachment)
-mail.Send()
