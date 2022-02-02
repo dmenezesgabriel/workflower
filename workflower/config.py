55260a486cbc76883fd9059669b23e3b303fcdc4
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-02 18:41:37 -0300

    Update default configuration to point to data directory

--- workflower/config.py
+++ workflower/config.py
@@ -13,9 +13,27 @@ class Config:
     DATA_DIR = os.getenv("DATA_DIR", "./data")
     APP_DATABASE_URL = os.getenv("APP_DATABASE_URL", "sqlite://")
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-    LOGGING_PATH = os.getenv("LOGGING_PATH", "/data/logs")
+    LOGGING_PATH = os.getenv(
+        "LOGGING_PATH",
+        os.path.join(
+            DATA_DIR,
+            "logs",
+        ),
+    )
     LOGGING_FILE = os.getenv("LOGGING_FILE", "log.log")
-    ENVIRONMENTS_DIR = os.getenv("ENVIRONMENTS_DIR", "/data/environments")
-    KERNELS_SPECS_DIR = os.getenv("KERNELS_SPECS_DIR", "/data/kernel_specs")
+    ENVIRONMENTS_DIR = os.getenv(
+        "ENVIRONMENTS_DIR",
+        os.path.join(
+            DATA_DIR,
+            "environments",
+        ),
+    )
+    KERNELS_SPECS_DIR = os.getenv(
+        "KERNELS_SPECS_DIR",
+        os.path.join(
+            DATA_DIR,
+            "kernel_specs",
+        ),
+    )
     PIP_INDEX_URL = os.getenv("PIP_INDEX_URL", None)
     PIP_TRUSTED_HOST = os.getenv("PIP_TRUSTED_HOST", None)
