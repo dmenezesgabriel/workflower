b0a2a0ab0fa8e0a5c5d7807ddfb5be2e22ba0171
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-02 08:33:21 -0300

    Update init_db

--- init_db.py
+++ init_db.py
@@ -3,19 +3,25 @@ import os
 
 from config import Config
 from workflower.models.base import BaseModel, database
+
+# Models must be imported even if unused so the tables can be declared.
 from workflower.models.event import Event
 from workflower.models.job import Job
 from workflower.models.workflow import Workflow
 
-logger = logging.getLogger("sqlalchemy")
-logger.setLevel(logging.DEBUG)
+sqlalchemy_logger = logging.getLogger("sqlalchemy")
+sqlalchemy_logger.setLevel(logging.DEBUG)
+
+logger = logging.getLogger("Init_db")
+logger.setLevel(logging.INFO)
+
 
 if __name__ == "__main__":
 
     if not os.path.isdir(Config.DATA_DIR):
         os.makedirs(Config.DATA_DIR)
     database.connect()
-    print(database.engine)
+    logger.debug(f"Using engine: {database.engine}")
 
     try:
         with database.session_scope() as session:
