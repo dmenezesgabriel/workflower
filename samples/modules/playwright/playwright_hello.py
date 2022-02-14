4a0e3e8147839a103386dab5fc2287331fce2b02
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-14 17:10:06 -0300

    Add playwright sample

---
+++ samples/modules/playwright/playwright_hello.py
@@ -0,0 +1,18 @@
+from workflower.modules.module import BaseModule
+
+from playwright.sync_api import sync_playwright
+
+
+class Module(BaseModule):
+    def __init__(self, plugins=None) -> None:
+        self._plugins = plugins
+
+    def run(self, *args, **kwargs):
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto("http://playwright.dev")
+            print(50 * "=")
+            print(page.title())
+            print(50 * "=")
+            browser.close()
