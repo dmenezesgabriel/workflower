c239ad1210edb37de47a89d23c3871b6b363c6d8
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-15 14:44:28 -0300

    Update run sh to be more user friendly

--- run.sh
+++ run.sh
@@ -7,61 +7,86 @@ set -e
 VERSION=0.1.0
 NAME="CLI Helper"
 
-echo "========================"
-echo "Hello $(whoami)",
-echo "Welcome to $NAME"
-echo "========================"
+echo "==========================================================="
+echo "Hello $(whoami),"
+echo "Welcome to $NAME, version $VERSION"
+echo "==========================================================="
+echo "Commands:"
+echo "- setup: create ~/environments/workflower"
+echo "- dev: init database and run app with dev config"
+echo "- prod: init database and run app with prod config"
+echo "- env: init database and run app with .env config"
+echo "- test: run tests"
+echo "- exit: exit program"
+echo "==========================================================="
 
-if [ $1 == "clean" ];
-then
-    echo "Removing development sqlite databases"
-    find . -name "*dev.sqlite*" -type f -delete
-    echo "Removing notebooks outputs"
-    find . -name "*output.ipynb" -type f -delete
-    echo "Removing logs"
-    find . -name "*.log*" -type f -delete
-# =========================================================================== #
-# Setup env
-# =========================================================================== #
-elif [ $1 == "setup" ];
-then
-    echo "Setup"
-    python -m venv ~/environments/workflower
-    echo "$(ls -lh ~/environments/workflower)"
-# =========================================================================== #
-# Run development environment
-# =========================================================================== #
-elif [ $1 == "dev" ];
-then
-    echo "Run dev"
-    eval "$(cat .env.dev.template)"  && \
-    python . init-db && \
-    python . run
-# =========================================================================== #
-# Run tests environment
-# =========================================================================== #
-elif [ $1 == "test" ];
-then
-    echo "Run test"
-    eval "$(cat .env.test.template)" && \
-    python -m pytest -s tests/
-# =========================================================================== #
-# Run production environment
-# =========================================================================== #
-elif [ $1 == "prod" ];
-then
-    echo "Run prod"
-    eval "$(cat .env.prod.template)"  && \
-    python . run
-# =========================================================================== #
-# Run default .env environment
-# =========================================================================== #
-elif [ $1 == "env" ];
-then
-    echo "Run with .env"
-    eval "$(cat .env)"  && \
-    python . init-db && \
-    python . run
-else
-    echo "No arg received ending"
-fi
+# Check platform
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+${machine}
+
+while true; do
+    read -p "waiting command... " cmd
+    if [ $cmd == "clean" ];
+    then
+        echo "Removing development sqlite databases"
+        find . -name "*dev.sqlite*" -type f -delete
+        echo "Removing notebooks outputs"
+        find . -name "*output.ipynb" -type f -delete
+        echo "Removing logs"
+        find . -name "*.log*" -type f -delete
+    # ======================================================================= #
+    # Setup env
+    # ======================================================================= #
+    elif [ $cmd == "setup" ];
+    then
+        echo "Setup"
+        python -m venv ~/environments/workflower
+        echo "$(ls -lh ~/environments/workflower)"
+    # ======================================================================= #
+    # Run development environment
+    # ======================================================================= #
+    elif [ $cmd == "dev" ];
+    then
+        echo "Run dev"
+        eval "$(cat .env.dev.template)"  && \
+        python . init-db && \
+        python . run
+    # ======================================================================= #
+    # Run tests environment
+    # ======================================================================= #
+    elif [ $cmd == "test" ];
+    then
+        echo "Run test"
+        eval "$(cat .env.test.template)" && \
+        python -m pytest -s tests/
+    # ======================================================================= #
+    # Run production environment
+    # ======================================================================= #
+    elif [ $cmd == "prod" ];
+    then
+        echo "Run prod"
+        eval "$(cat .env.prod.template)"  && \
+        python . run
+    # ======================================================================= #
+    # Run default .env environment
+    # ======================================================================= #
+    elif [ $cmd == "env" ];
+    then
+        echo "Run with .env"
+        eval "$(cat .env)"  && \
+        python . init-db && \
+        python . run
+    elif [ $cmd == "exit" ];
+    then
+        exit
+    else
+        echo "No arg received ending"
+    fi
+done
