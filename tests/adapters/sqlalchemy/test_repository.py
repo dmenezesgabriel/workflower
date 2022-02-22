68d3ad58bffd5143af95a151c6572185b2c171a7
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-22 19:14:29 -0300

    Add repository delete test

--- tests/adapters/sqlalchemy/test_repository.py
+++ tests/adapters/sqlalchemy/test_repository.py
@@ -7,23 +7,37 @@ from workflower.domain.entities.workflow import Workflow
 
 class TestSqlAlchemyRepository:
     def test_repository_can_save_an_entity(self, session):
+
+        assert session.query(Workflow).all() == []
+
         workflow = Workflow(name="test")
         repository = SqlAlchemyRepository(session, model=Workflow)
         repository.add(workflow)
         session.commit()
 
+        assert len(session.query(Workflow).all()) == 1
+
         record = session.query(Workflow).first()
         assert record.name == "test"
 
     def test_repository_returns_an_entity(self, session):
+
+        assert session.query(Workflow).all() == []
+
         workflow = Workflow(name="test")
         session.add(workflow)
         session.commit()
 
         repository = SqlAlchemyRepository(session, model=Workflow)
+
+        assert len(session.query(Workflow).all()) == 1
         assert workflow.id == repository.get(id=workflow.id).id
 
     def test_repository_returns_an_entity_with_relationships(self, session):
+
+        assert session.query(Workflow).all() == []
+        assert session.query(Job).all() == []
+
         job = Job(
             name="test",
             operator="python",
@@ -37,12 +51,19 @@ class TestSqlAlchemyRepository:
         session.commit()
 
         repository = SqlAlchemyRepository(session, model=Workflow)
+
+        assert len(session.query(Workflow).all()) == 1
+        assert len(session.query(Job).all()) == 1
+
         record = repository.get(id=workflow.id)
         assert record.jobs[0].name == "test"
         assert record.jobs[0].operator == "python"
         assert record.jobs[0].definition == {"trigger": "date"}
 
     def test_repository_return_a_list_of_entities(self, session):
+
+        assert session.query(Workflow).all() == []
+
         session.execute(
             """
             INSERT INTO workflow (name) VALUES
@@ -54,6 +75,9 @@ class TestSqlAlchemyRepository:
         session.commit()
 
         repository = SqlAlchemyRepository(session, model=Workflow)
+
+        assert len(session.query(Workflow).all()) == 3
+
         workflows = repository.list()
         assert workflows[0].name == "test0"
         assert workflows[1].name == "test1"
@@ -76,3 +100,8 @@ class TestSqlAlchemyRepository:
 
         new_session = session_factory()
         repository = SqlAlchemyRepository(session, model=Workflow)
+        workflow = repository.get(id=1)
+        repository.remove(workflow)
+        new_session.commit()
+
+        assert session.query(Workflow).all() == []
