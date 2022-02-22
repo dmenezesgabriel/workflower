9200269f05d0b237cd6937b97067e88457500578
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-22 19:10:12 -0300

    Add entities count verification to orm test

--- tests/adapters/sqlalchemy/test_orm.py
+++ tests/adapters/sqlalchemy/test_orm.py
@@ -4,6 +4,9 @@ from workflower.domain.entities.workflow import Workflow
 
 class TestWorkflowMapper:
     def test_workflow_mapper_can_load_workflows(self, session):
+
+        assert session.query(Workflow).all() == []
+
         session.execute(
             """
             INSERT INTO workflow (name) VALUES
@@ -13,20 +16,32 @@ class TestWorkflowMapper:
             """
         )
 
+        assert len(session.query(Workflow).all()) == 3
+
         workflows = session.query(Workflow).all()
+
         assert workflows[0].name == "test0"
         assert workflows[1].name == "test1"
         assert workflows[2].name == "test2"
 
     def test_workflow_mapper_can_save_workflow(self, session):
+
+        assert session.query(Workflow).all() == []
+
         workflow = Workflow(name="test")
         session.add(workflow)
         session.commit()
+
+        assert len(session.query(Workflow).all()) == 1
 
         record = session.query(Workflow).first()
         assert record.name == "test"
 
     def test_workflow_mapper_can_save_workflow_jobs(self, session):
+
+        assert session.query(Workflow).all() == []
+        assert session.query(Job).all() == []
+
         job = Job(
             name="test",
             operator="python",
@@ -39,7 +54,11 @@ class TestWorkflowMapper:
         session.add(workflow)
         session.commit()
 
+        assert len(session.query(Workflow).all()) == 1
+        assert len(session.query(Job).all()) == 1
+
         record = session.query(Workflow).first()
+
         assert record.jobs[0].name == "test"
         assert record.jobs[0].operator == "python"
         assert record.jobs[0].definition == {"trigger": "date"}
@@ -47,30 +66,46 @@ class TestWorkflowMapper:
 
 class TestJobMapper:
     def test_job_mapper_can_load_jobs(self, session):
+
+        assert session.query(Job).all() == []
+
         session.execute(
             """
             INSERT INTO job (name, operator, definition) VALUES
             ("test0", "python", '{"trigger": "date"}')
             """
         )
+
+        assert len(session.query(Job).all()) == 1
+
         jobs = session.query(Job).all()
         assert jobs[0].name == "test0"
         assert jobs[0].operator == "python"
         assert jobs[0].definition == {"trigger": "date"}
 
     def test_job_mapper_can_save_jobs(self, session):
+
+        assert session.query(Job).all() == []
+
         job = Job(
             name="test", operator="python", definition={"trigger": "date"}
         )
         session.add(job)
         session.commit()
 
+        assert len(session.query(Job).all()) == 1
+
         record = session.query(Job).first()
+
         assert record.name == "test"
         assert record.operator == "python"
         assert record.definition == {"trigger": "date"}
 
     def test_job_mapper_backpopulate_workflow(self, session):
+
+        assert session.query(Workflow).all() == []
+        assert session.query(Job).all() == []
+
         workflow = Workflow(name="test")
         session.add(workflow)
         session.commit()
@@ -83,6 +118,9 @@ class TestJobMapper:
         )
         session.add(job)
         session.commit()
+
+        assert len(session.query(Workflow).all()) == 1
+        assert len(session.query(Job).all()) == 1
 
         record = session.query(Workflow).first()
         assert record.jobs[0].name == "test"
--- tests/adapters/sqlalchemy/test_repository.py
+++ tests/adapters/sqlalchemy/test_repository.py
@@ -1,3 +1,5 @@
+from re import A
+
 from workflower.adapters.sqlalchemy.repository import SqlAlchemyRepository
 from workflower.domain.entities.job import Job
 from workflower.domain.entities.workflow import Workflow
@@ -20,3 +22,57 @@ class TestSqlAlchemyRepository:
 
         repository = SqlAlchemyRepository(session, model=Workflow)
         assert workflow.id == repository.get(id=workflow.id).id
+
+    def test_repository_returns_an_entity_with_relationships(self, session):
+        job = Job(
+            name="test",
+            operator="python",
+            definition={"trigger": "date"},
+        )
+
+        workflow = Workflow(name="test")
+        workflow.add_job(job)
+
+        session.add(workflow)
+        session.commit()
+
+        repository = SqlAlchemyRepository(session, model=Workflow)
+        record = repository.get(id=workflow.id)
+        assert record.jobs[0].name == "test"
+        assert record.jobs[0].operator == "python"
+        assert record.jobs[0].definition == {"trigger": "date"}
+
+    def test_repository_return_a_list_of_entities(self, session):
+        session.execute(
+            """
+            INSERT INTO workflow (name) VALUES
+            ("test0"),
+            ("test1"),
+            ("test2")
+            """
+        )
+        session.commit()
+
+        repository = SqlAlchemyRepository(session, model=Workflow)
+        workflows = repository.list()
+        assert workflows[0].name == "test0"
+        assert workflows[1].name == "test1"
+        assert workflows[2].name == "test2"
+
+    def test_repository_deletes_entity(self, session_factory):
+        session = session_factory()
+
+        assert session.query(Workflow).all() == []
+
+        session.execute(
+            """
+            INSERT INTO workflow (name) VALUES
+            ("test0")
+            """
+        )
+        session.commit()
+
+        assert len(session.query(Workflow).all()) == 1
+
+        new_session = session_factory()
+        repository = SqlAlchemyRepository(session, model=Workflow)
