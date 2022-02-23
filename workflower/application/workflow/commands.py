265ff05d0dab20293ad1ec2d75fa2dc1ab045f7b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-23 19:41:04 -0300

    Add create workflow command

---
+++ workflower/application/workflow/commands.py
@@ -0,0 +1,41 @@
+import logging
+from typing import List
+
+from sqlalchemy.exc import IntegrityError
+from workflower.application.interfaces.unit_of_work import UnitOfWork
+from workflower.domain.entities.workflow import Workflow
+from workflower.models.job import Job
+
+logger = logging.getLogger("workflower.application.workflow.commands")
+
+
+class CreateWorkflowCommand:
+    def __init__(self, unit_of_work: UnitOfWork) -> None:
+        self.unit_of_work = unit_of_work
+
+    def execute(
+        self,
+        name: str,
+        is_active: bool = True,
+        file_path: str = None,
+        file_exists: bool = None,
+        file_last_modified_at: str = None,
+        modified_since_last_load: bool = False,
+        jobs: List[Job] = None,
+    ):
+        try:
+            with self.unit_of_work as uow:
+                workflow = Workflow(
+                    name,
+                    is_active,
+                    file_path,
+                    file_exists,
+                    file_last_modified_at,
+                    modified_since_last_load,
+                    jobs,
+                )
+                uow.workflows.add(workflow)
+        except IntegrityError as e:
+            logger.error(f"Integrity error: {e}")
+        except Exception as e:
+            logger.error(f"Error: {e}")
--- workflower/domain/entities/event.py
+++ workflower/domain/entities/event.py
@@ -1,18 +1,6 @@
 import logging
 
-from workflower.controllers.job import JobController
-from workflower.models.job import Job
-from workflower.utils import crud
-
 logger = logging.getLogger("workflower.models.event")
-
-
-def _update_job(session, event, scheduler):
-    Job.update_next_run_time(session, event.job_id, scheduler)
-    logger.debug("Checking if need to trigger a dependency job")
-    JobController.trigger_dependencies(
-        session, event.job_id, scheduler, job_return_value=event.retval
-    )
 
 
 class Event:
@@ -33,55 +21,3 @@ class Event:
             f"model={self.model}, "
             f"model_id={self.model_id})"
         )
-
-    @classmethod
-    def job_added(cls, session, event):
-        """
-        On job added.
-        """
-        job = crud.get_one(session, Job, id=event.job_id)
-        crud.create(
-            session, cls, name="job_added", model="job", model_id=job.id
-        )
-
-    @classmethod
-    def job_removed(cls, session, event):
-        """
-        On job removed.
-        """
-        job = crud.get_one(session, Job, id=event.job_id)
-        crud.create(
-            session, cls, name="job_removed", model="job", model_id=job.id
-        )
-
-    @classmethod
-    def job_error(cls, session, event) -> None:
-        """
-        On job error.
-        """
-        job = crud.get_one(session, Job, id=event.job_id)
-        crud.create(
-            session,
-            cls,
-            name="job_error",
-            model="job",
-            model_id=job.id,
-            exception=event.exception,
-        )
-
-    @classmethod
-    def job_executed(cls, session, event, scheduler) -> None:
-        """
-        On job executed.
-        """
-        logger.info(f"Job: {event.job_id}, successfully executed")
-        job = crud.get_one(session, Job, id=event.job_id)
-        crud.create(
-            session,
-            cls,
-            name="job_executed",
-            model="job",
-            model_id=job.id,
-            output=event.retval,
-        )
-        _update_job(session, event, scheduler)
