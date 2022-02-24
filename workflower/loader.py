a2ca56a79f9b96828b5c81aa0b4d57f53e768331
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-24 02:08:04 -0300

    Update workflow loader

--- workflower/loader.py
+++ workflower/loader.py
@@ -1,43 +1,15 @@
 import logging
 import os
 import traceback
-from abc import ABC, abstractclassmethod
 from typing import List
 
+from workflower.application.workflow.commands import (
+    LoadWorkflowFromYamlFileCommand,
+)
 from workflower.config import Config
 from workflower.domain.entities.workflow import Workflow
-from workflower.schema.parser import WorkflowSchemaParser
-from workflower.utils.file import yaml_file_to_dict
 
 logger = logging.getLogger("workflower.loader")
-
-
-class LoadStrategy(ABC):
-    @abstractclassmethod
-    def load(self):
-        raise NotImplementedError
-
-
-class YmlLoadStrategy(LoadStrategy):
-    def load(self, file_path: str):
-        return yaml_file_to_dict(file_path)
-
-
-class WorkflowFileLoader:
-    def __init__(self, strategy: LoadStrategy):
-        self._strategy = strategy
-
-    @property
-    def strategy(self):
-        return self._strategy
-
-    @strategy.setter
-    def strategy(self, strategy: LoadStrategy):
-        self._strategy = strategy
-
-    def load_workflow(self, file_path: str):
-        WorkflowSchemaParser()
-        self._strategy.load(file_path)
 
 
 class WorkflowLoader:
@@ -48,17 +20,16 @@ class WorkflowLoader:
     def workflows(self) -> List[Workflow]:
         return self._workflows
 
-    def load_one_from_file(
-        self, session, workflow_yaml_config_path: str
-    ) -> Workflow:
+    def load_one_from_file(self, uow, file_path: str) -> Workflow:
         """
         Load one workflow from a yaml file.
         """
-        logger.info(f"Loading pipeline file: {workflow_yaml_config_path}")
-        return Workflow.from_yaml(session, workflow_yaml_config_path)
+        logger.info(f"Loading pipeline file: {file_path}")
+        command = LoadWorkflowFromYamlFileCommand(uow, file_path)
+        return command.execute()
 
     def load_all_from_dir(
-        self, session, workflows_path: str = Config.WORKFLOWS_FILES_PATH
+        self, uow, workflows_path: str = Config.WORKFLOWS_FILES_PATH
     ) -> List[Workflow]:
         """
         Load all.
@@ -72,7 +43,7 @@ class WorkflowLoader:
                     workflow_yaml_config_path = os.path.join(root, file)
                     try:
                         workflow = self.load_one_from_file(
-                            session, workflow_yaml_config_path
+                            uow, workflow_yaml_config_path
                         )
                         counter += 1
                         if workflow:
